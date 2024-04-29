a179844fafb5f487b6c7825f8303561ca7f23aba
Yash Bhalchandra <yashb1004@gmail.com>
2024-04-29 08:42:11 +0530

    deployment prep

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: 'img/y-website-favicon-black.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://yashbhal.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/personal-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yashbhal', // Usually your GitHub org/user name.
+  projectName: 'personal-website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
