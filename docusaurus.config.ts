d98a7f88bb3e2cfdf8e0a68d383401e6e48ae533
Yash Bhalchandra <yashb1004@gmail.com>
2024-04-30 10:43:57 +0530

    removed excess code

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,6 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
         },
         theme: {
           customCss: './src/css/custom.css',
